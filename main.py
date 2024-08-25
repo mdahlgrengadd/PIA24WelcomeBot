ee052c9bfab32021e70d8faa5c91a6baeab963aa
Martin Dahlgren <mdahlgrengadd@users.noreply.github.com>
2024-08-25 11:05:30 +0200

    Rewrite and simplified, SD image generation

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-from responses import get_response
+from image_response import generate_image
 import json
 import os
 
-# Get configuration.json
+# Okänslig konfig från configuration.json
 with open("configuration.json", "r") as config: 
 	data = json.load(config)
-	load_dotenv()
-	token = os.getenv('DISCORD_TOKEN') #data["token"]
-	prefix = data["prefix"]
+
+	prefix = data["prefix"] # Vilket prefix som används för kommandon
 	owner_id = data["owner_id"]
 
+	# Ladda känslig konfig som exvis 'token' från en .env fil
+	load_dotenv()
+	token = os.getenv('DISCORD_TOKEN')
 
 class Greetings(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self._last_member = None
 
-# Intents
+# Intents - Vad vill kunna göra
 intents = discord.Intents.default()
 intents.message_content = True
-# The bot
-bot = commands.Bot(prefix, intents = intents, owner_id = owner_id)
+intents.members = True
 
-# Load cogs
+bot = commands.Bot(command_prefix=prefix, intents = intents, owner_id = owner_id)
+
 if __name__ == '__main__':
 	for filename in os.listdir("Cogs"):
 		if filename.endswith(".py"):
@@ -33,39 +35,24 @@ if __name__ == '__main__':
 
 @bot.event
 async def on_ready():
-	print(f"We have logged in as {bot.user}")
+	print(f"Inloggad som {bot.user}")
 	print(discord.__version__)
 	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
 
-async def send_message(message: discord.Message, user_message:str) -> None:
-	if not user_message:
-		print('(Message empty. Is intents enabled?)')
-		return
-	if is_private := user_message[0] == '?':
-		user_message = user_message[1:]
-
-	try:
-		response = get_response(user_message)
-		await message.author.send(response) if is_private else await message.channel.send(response)
-	except Exception(e):
-		print(e)
-
-@bot.event
-async def on_message(message: discord.Message) -> None:
-	if message.author == bot.user:
-		return
-	
-	username = str(message.author)
-	user_message = message.content
-	channel = str(message.channel)
-
-	print(f'[{channel}] {username}: "{user_message}')
-	await send_message(message, user_message)
-
+# Hälsa användare välkomna med ett privat meddelande
 @bot.event
 async def on_member_join(member):
-	#channel = bot.get_channel(790274325533378682)
-	#embed=discord.Embed(title="Welcome!",description=f"{member.mention} Just Joined")
 	await member.send(f'Välkommen {member.mention}! Hoppas du ska trivas!')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hej, {ctx.author.name}!')
+
+# Generera en bild med hjälp av stable diffusion
+@bot.command()
+async def generate(ctx):
+	# Tar för mkt datorkraft
+	#await generate_image(ctx)
+	return
 
 bot.run(token)
--- responses.py
+++
@@ -1,9 +0,0 @@
-from random import choice, randint
-
-def get_response(user_input: str) -> str:
-    lowered = user_input.lower()
-
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'hello' in lowered:
-        return 'Hello there!'
